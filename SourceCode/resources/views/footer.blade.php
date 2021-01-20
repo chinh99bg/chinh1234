6bd79a00358e7d76a332e2eea850d05f38e6d72f
chinh99bg <chinhhd72@wru.vn>
2021-01-20 23:30:39 +0700

    first commit

---
+++ SourceCode/resources/views/footer.blade.php
@@ -0,0 +1,42 @@
+<div class="wrap">
+    <div style="background-color: #4b7d9b; height: 230px;">
+        <div>
+            <div style="height: 200px; float: left; margin-left: 14.5%; padding-top: 23px;">
+                <div style="color: white; font-size: 17px; font-weight: bold;">
+                    <p>About Us</p>
+                </div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">About Us</a></div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">Whuy Us</a></div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">Customer Stories</a></div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">Press Resources</a></div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">Press releases</a></div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">Contact Us</a></div>
+            </div>
+
+            <div style="float: right; padding-right: 65.2%; margin-top: 25px">
+                <div style="color: white; font-size: 17px; font-weight: bold; ">
+                    <p>Proparties</p>
+                </div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">Rent a home</a></div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">Lease a home</a></div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">Buy a home</a></div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">Sell a home</a></div>
+                <div><a href="#" style="color: white; font-size: 12px;line-height: 2">Ask advices</a></div>
+            </div>
+
+            <div style="height: 200px; float: right; margin-top: -153px; padding-right: 37.2%;">
+                <div style="color: white; font-size: 17px;font-weight: bold; ">
+                    Costamer Testimonials
+                    <img src="/source/images/layer39.png" alt="Alternate Text" style="padding-left:10px" />
+                    <img src="/source/images/layer391.png" alt="Alternate Text" style="padding-left:5px" />
+                </div>
+                <p style="color: white; width:275px;font-size: 12px; padding-top: 10px; line-height: 2">
+                    Lorem Ipsum is simply dummy text of the printting and typesetting industry. Lorem Ipsum has the industry standard dummy text ever since the 1500s, when an unknown printer took a.
+                    <p style="width: 52%; color: white; font-weight: bold; font-size: 13px; line-height: 1.8">
+                        Lorem Ipsum is simply Owner, Lorem Ipsum
+                    </p>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
